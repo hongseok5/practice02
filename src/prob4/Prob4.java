af776d8b0fb953983f3eb1d9ced55c0ee4f0b179
BIT <BIT@DESKTOP-2O251SG>
2017-08-02 10:01:06 +0900

    correct error

--- src/prob4/Prob4.java
+++ src/prob4/Prob4.java
@@ -1,7 +1,14 @@
 package prob4;
+
 public class Prob4 {
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
+		
 		char[] c1 = reverse( "Hello World" );
 		printCharArray( c1 );
 		
@@ -9,25 +16,27 @@ public class Prob4 {
 		printCharArray( c2 );
 	}
 	
+	
+	
+	
+	
+	
 	public static char[] reverse(String str) {  // 스트링을 입력받아 문자 순서를 뒤집어 char[]로 반환 
 		
 		
-		
-		
-		/* 코드를 완성합니다 */
+		//스트링을 스트링으로 반환하고(순서바꿔서) 문자형 배열로 바꾸고 출력 
 		char[] src = new char[str.length()];  // 순서 바뀌기 전 스트링을 char 배열로 생성 
-
+		/* 코드를 완성합니다 */
+		for(int i = 0; i<str.length();i++) {
 		
-		for(int i=0; i<str.length();i++) {
-			int j = str.length();
-			src[i]= str.charAt(j-1);
-			j--;
-			
+		src[i]=str.charAt(str.length()-1-i);
+		//String.valueOf(arg0) // 문자 배열을 받아 스트링으로 변환 
 		}
 		
-		
-			
 		return src;
+		
+
+
 	}
 
 	public static void printCharArray(char[] array){
